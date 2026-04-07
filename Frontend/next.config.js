64b5df81c48d9eb3c2322e674a60702c61b2f27c
Pruthviraj_Ghatul 2220220350 <pruthviraj.g@somaiya.edu>
2026-04-07 13:52:10 +0530

    added render backend

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   async rewrites() {
     return [
-      // Proxy API calls to backend to avoid CORS issues in dev.
+      
       {
         source: "/api/voice-chat",
-        destination: "http://localhost:3000/api/voice-chat",
+        destination: "https://doctorsai.onrender.com/api/voice-chat",
       },
     ];
   },
